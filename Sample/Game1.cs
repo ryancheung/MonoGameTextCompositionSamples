53745b6c20417ad16ec37d5433a55c42855bbdfb
ryancheung <ryancheung.go@gmail.com>
2020-08-29 12:08:44 +0800

    Filter control key text input events

--- Sample/Game1.cs
+++ Sample/Game1.cs
@@ -77,6 +77,9 @@ namespace Sample
                         inputContent = "";
                         break;
                     default:
+                        if (char.IsControl(e.Character))
+                            break;
+
                         inputContent += e.Character;
                         break;
                 }
